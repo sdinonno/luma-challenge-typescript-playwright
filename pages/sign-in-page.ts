e9ff7f5e888d5b25bef5f51b6c364c7761911405
Sofia Di Nonno <sofidinonno@gmail.com>
2023-10-09 16:43:09 -0300

    Update Sign In page - Add constructor

--- pages/sign-in-page.ts
+++ pages/sign-in-page.ts
@@ -7,6 +7,13 @@ class SignInPage {
     readonly signInButton: Locator;
     readonly forgotPasswordLink: Locator;
     readonly createAccountButton: Locator;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.email = page.locator("#email");
+        this.password = page.locator("#pass");
+        this.signInButton = page.locator("#send2")
+    }
     
     async fillEmail(email: string) {
         this.email.fill(email);
@@ -21,4 +28,11 @@ class SignInPage {
         this.fillPassword(password);
         this.signInButton.click();
     }
+
+    async checkLoggedIn() {
+        await expect(this.page).toHaveURL(/.*account/);
+        await expect(this.page).toHaveTitle("My Account");
+    }
 }
+
+export default SignInPage;
