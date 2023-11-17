047197f4233b95d196939161d87ab7efeffcc58d
Sofia Di Nonno <sofidinonno@gmail.com>
2023-11-16 23:28:48 -0300

    Update home-page class

--- pages/home-page.ts
+++ pages/home-page.ts
@@ -3,19 +3,21 @@ import { type Locator, type Page, expect } from '@playwright/test';
 class HomePage {
     readonly page: Page;
     readonly searchInput: Locator;
+    readonly navigationBarOptions: Locator;
 
     constructor(page: Page) {
         this.page = page;
         this.searchInput = page.locator("#search");
+        this.navigationBarOptions = page.locator("ul#ui-id-2 > li");
     }
 
     async fillSearchInput(keywords: string) {
-        this.searchInput.fill(keywords);
+        await this.searchInput.fill(keywords);
     }
 
     async search(keywords: string) {
-        this.fillSearchInput(keywords);
-        this.searchInput.press("Enter");
+        await this.fillSearchInput(keywords);
+        await this.searchInput.press("Enter");
     }
 }
 
