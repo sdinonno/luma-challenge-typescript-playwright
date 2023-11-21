58a13c142250922b87c9c02281bee359ed0e9431
Gul Najeeb Channa <najeeb.capricorn@gmail.com>
2023-11-21 16:48:23 +0100

    Added test to get latest attire available on the homepage.

--- pages/home-page.ts
+++ pages/home-page.ts
@@ -1,6 +1,12 @@
 import { type Locator, type Page, expect } from '@playwright/test';
 
 class HomePage {
+
+    homePageLocators = {
+        latestAttiresItemsDiv:() => this.page.locator("//div[@class='products-grid grid']")
+    }
+
+
     readonly page: Page;
     readonly searchInput: Locator;
     readonly navigationBarOptions: Locator;
@@ -18,6 +24,19 @@ class HomePage {
     async search(keywords: string) {
         await this.fillSearchInput(keywords);
         await this.searchInput.press("Enter");
+    }
+
+    public async getLatestAttire():Promise<void>{
+        await expect(this.homePageLocators.latestAttiresItemsDiv()).toBeVisible();
+        const latestAttires = await this.page.locator("//div[@class='products-grid grid']//li").count()
+        console.log("\n" + "Number of latest attires found -> " + latestAttires)
+        for(let i=1;i<=latestAttires;i++){
+            const getListOfLatestAttires = this.page.locator("//div[@class='products-grid grid']//li[placeholder]//strong".replace('placeholder', i.toString()));
+            const items = await getListOfLatestAttires.textContent();
+            const textOutput = [items]
+            process.stdout.write((i) + "-> " + `${textOutput}`.trim());
+        }
+        
     }
 }
 
--- tests/signIn.spec.ts
+++ tests/signIn.spec.ts
@@ -2,6 +2,7 @@ import { test } from '@playwright/test';
 import SignInPage from '../pages/sign-in-page';
 import hooks from '../utils/hooks';
 import pages from '../utils/pagesUrl';
+import HomePage from '../pages/home-page';
 
 let signInPage: SignInPage;
 
@@ -18,5 +19,15 @@ test.describe('LUMA Website - Sign In', () => {
     test('successfull login', async () => {
         await signInPage.doLogin('ontopcontract@gmail.com', '123456J.');
         await signInPage.checkLoggedIn();
-    })
-})
+    });
+});
+
+test.describe('Get latest attire', () => {
+    test('Get latest attire', async ({page}) => {
+        await signInPage.doLogin('ontopcontract@gmail.com', '123456J.');
+        await signInPage.checkLoggedIn();
+        await page.goto('https://magento.softwaretestingboard.com/what-is-new.html')
+        let homePage = new HomePage(page);
+        await homePage.getLatestAttire();
+    });
+});
