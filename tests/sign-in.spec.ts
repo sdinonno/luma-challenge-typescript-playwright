6b63da8da0520599d99b2a88f9198b6ac2839248
Sofia Di Nonno <sofidinonno@gmail.com>
2023-11-29 20:41:30 -0300

    Renamed sign in spec

--- tests/sign-in.spec.ts
+++ tests/sign-in.spec.ts
@@ -1,8 +1,9 @@
 import { test } from '@playwright/test';
 import SignInPage from '../pages/sign-in-page';
-import hooks from '../utils/hooks';
 import pages from '../utils/pagesUrl';
 
+const userName = process.env.USERNAME!;
+const password = process.env.PASSWORD!;
 let signInPage: SignInPage;
 
 test.use({ storageState: { cookies: [], origins: [] } });
@@ -11,12 +12,16 @@ test.describe.configure({ mode: 'serial' });
 test.beforeEach(async ({ page }) => {
     await page.goto(pages.signIn);
     signInPage = new SignInPage(page);
-
 });
 
 test.describe('LUMA Website - Sign In', () => {
     test('successfull login', async () => {
-        await signInPage.doLogin('ontopcontract@gmail.com', '123456J.');
+        await signInPage.doLogin(userName, password);
         await signInPage.checkLoggedIn();
+    })
+
+    test('failed login - invalid password ', async() => {
+        await signInPage.doLogin(userName, 'dfisj');
+        await signInPage.checkInvalidLogin();
     })
 })
