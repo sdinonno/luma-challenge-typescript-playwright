0073508ea127bd9f3a347b323bdd5e690423187d
Sofia Di Nonno <sofidinonno@gmail.com>
2023-11-29 20:42:17 -0300

    New sign up spec with successfully scenario

---
+++ tests/sign-up.spec.ts
@@ -0,0 +1,30 @@
+import { test } from '@playwright/test';
+import SignUpPage from '../pages/sign-up-page';
+import pages from '../utils/pagesUrl';
+import DataGeneratorClass from '../utils/data-generator';
+
+let signUpPage: SignUpPage;
+let dataGenerator: DataGeneratorClass;
+
+test.use({ storageState: { cookies: [], origins: [] } });
+test.describe.configure({ mode: 'serial' });
+
+test.beforeEach(async ({ page }) => {
+    await page.goto(pages.signUp);
+    signUpPage = new SignUpPage(page);
+    dataGenerator = new DataGeneratorClass();
+});
+
+test.describe('LUMA Website - Sign Up', () => {
+    test('successfull sign up', async () => {
+        const password = dataGenerator.generatePassword()
+
+        await signUpPage.fillAccountForm(dataGenerator.generateName(),
+            dataGenerator.generateLastName(),
+            dataGenerator.generateEmail(),
+            password,
+            password);
+        await signUpPage.checkSucessfullRegistration();
+    })
+})
+
